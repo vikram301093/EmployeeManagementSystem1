685f0c794d9dd886a68567e40abb746be162f599
vikram301093 <vikram.singhkushwaha@atos.net>
2017-03-23 15:10:16 +0530

    Update MyConnection.java

--- src/conn/MyConnection.java
+++ src/conn/MyConnection.java
@@ -6,8 +6,8 @@ public class MyConnection {
 
 	 private Connection con;
 	 private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
-	 private static final String URL = "jdbc:mysql://172.30.119.238:3306/simpledb";
-	 private static final String USER_NAME = "vikram";
+	 private static final String URL = "jdbc:mysql://mydbvikram.cl0uft8eytsx.ap-south-1.rds.amazonaws.com:3306/simpledb";
+	 private static final String USER_NAME = "root";
 	 private static final String PASSWORD = "lap725top";
 	 public Connection getMyConnnection(){
 	  try {
